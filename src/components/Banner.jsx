03723c35bba20950da65bfdd4ebaf7388a43d9ad
SM-IMON <imon.dvp@gmail.com>
2025-10-03 10:53:30 +0600

    The banner code is updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,7 @@ const BannerItem = ({ image, title, description }) => (
     <img src={image} alt={title} className="h-full w-full object-cover" />
     <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
       <div className="w-3/4 text-center md:w-2/4">
-        <Typography variant="h1" color="white" className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+        <Typography variant="h1" color="white" className="mb-4 text-2xl md:text-4xl lg:text-5xl">
           {title}
         </Typography>
         <Typography variant="lead" color="white" className="mb-12 opacity-80">
